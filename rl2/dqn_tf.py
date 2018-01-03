827493ea89a170540561ffacda6fd5fe61be6a28
Derek Li <liderek269@gmail.com>
2018-01-03 13:14:50 -0800

    epsilon greedy

---
+++ rl2/dqn_tf.py
@@ -0,0 +1,22 @@
+# https://deeplearningcourses.com/c/deep-reinforcement-learning-in-python
+# https://www.udemy.com/deep-reinforcement-learning-in-python
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+import copy
+import gym
+import os
+import sys
+import random
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from gym import wrappers
+from datetime import datetime
+from scipy.misc import imresize
+
+
+if __name__ == '__main__':
+    print("heloo world")
